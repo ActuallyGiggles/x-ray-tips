9f45c62f86bf1fd96f0ae92cf028602e86ed0165
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2023-02-08 23:44:57 -0800

    big changes

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const examCardTemplate = document.querySelector("[data-exam-template]")
 const examCardsContainer = document.querySelector("[data-exam-cards-container]")
 const searchInput = document.querySelector("[exam-search]")
 
+const page_name = document.getElementById("page_exam_name")
+const page_img = document.getElementById("page_img")
+const page_clinical_indications = document.getElementById("clinical_indications")
+const page_technical_factors = document.getElementById("technical_factors")
+const page_patient_position = document.getElementById("patient_position")
+const page_part_position = document.getElementById("part_position")
+const page_cr = document.getElementById("cr")
+const page_respiration = document.getElementById("respiration")
+const page_evaluation_criteria = document.getElementById("evaluation_criteria")
+
 let examSeries = []
+let exams = []
 
 searchInput.addEventListener("input", function (event) {
     const value = event.target.value.toLowerCase()
@@ -23,29 +34,87 @@ document.addEventListener("click", function (event) {
         for (const view of event.target.parentElement.parentElement.querySelector("[data-views-cards]").children) {
             view.classList.toggle("hide")
         }
+    } else if (event.target.className == "view_card") {
+        exams.forEach(exam => {
+            if (exam["Name"] == event.target.id) {
+                // Exam Name
+                page_name.innerHTML = exam["Name"]
+
+                // Exam Image
+                page_img.src = "./img/" + exam["Part"].toLowerCase() + "/" + exam["View"] + ".jpg"
+
+                // Clinical Indications
+                populateListFromArray(page_clinical_indications, exam["Clinical Indications"])
+
+                // Technical Factors
+                populateListFromObject(page_technical_factors, exam["Technical Factors"])
+
+                // Patient Position
+                populateListFromArray(page_patient_position, exam["Patient Position"])
+
+                // Part Position
+                populateListFromArray(page_part_position, exam["Part Position"])
+
+                // CR
+                populateListFromArray(page_cr, exam["CR"])
+
+                // Respiration
+                populateListFromArray(page_respiration, exam["Respiration"])
+
+                // Evaluation Criteria
+                populateListFromObject(page_evaluation_criteria, exam["Evaluation Criteria"])
+            }
+        });
+        document.getElementById("page_content").classList.remove("hide")
     }
 })
+
+function populateListFromArray(list, array) {
+    list.innerHTML = ""
+    array.forEach(item => {
+        var li = document.createElement("li")
+        li.innerText = item
+        list.appendChild(li)
+    })
+}
+
+function populateListFromObject(list, object) {
+    list.innerHTML = ""
+    for (const key in object) {
+        if (Object.hasOwnProperty.call(object, key)) {
+            const item = object[key];
+            var li = document.createElement("li")
+            li.innerText = key + ": " + item
+            list.appendChild(li)
+        }
+    }
+}
 
 fetch("./exams.json")
     .then((res) => res.json())
     .then((data) => {
-        examSeries = data.map(exam => {
+        examSeries = data.map(series => {
+            series["Exams"].forEach(element => {
+                exams.push(element)
+            });
+
             const exam_cards = examCardTemplate.content.cloneNode(true).children[0]
 
             const main_card = exam_cards.querySelector("[data-main-card]")
-            main_card.id = exam["Series"]
-            const series = main_card.querySelector("[data-main-card-name]")
-            const views = main_card.querySelector("[data-main-card-views]")
-            series.textContent = exam["Series"]
-            views.textContent = exam["Views"]
+            main_card.id = series["Part"]
+            const seriesElement = main_card.querySelector("[data-main-card-name]")
+            const viewsElement = main_card.querySelector("[data-main-card-views]")
+            seriesElement.textContent = series["Part"]
+            viewsElement.textContent = series["Views"]
 
             const views_cards = exam_cards.querySelector("[data-views-cards]")
-            exam["Views"].split(", ").forEach(view => {
+            series["Views"].split(", ").forEach(view => {
                 const node = document.createElement("div")
                 const textnode = document.createTextNode(view)
                 node.appendChild(textnode)
                 node.classList.add("hide")
                 node.classList.add("view_card")
+                node.id = view + " " + series["Part"]
                 views_cards.appendChild(node)
             });
 
@@ -53,6 +122,6 @@ fetch("./exams.json")
             exam_cards.appendChild(views_cards)
 
             examCardsContainer.append(exam_cards)
-            return { series: exam["Series"], views: exam["Views"], element: exam_cards }
+            return { series: series["Part"], views: series["Views"], element: exam_cards }
         });
     });
