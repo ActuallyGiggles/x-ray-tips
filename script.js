57a4416371c242539fa58b6294ebf8ad8cbf7d4c
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2023-02-08 20:36:32 -0800

    different views

--- script.js
+++ script.js
@@ -4,56 +4,55 @@ const searchInput = document.querySelector("[exam-search]")
 
 let examSeries = []
 
-const onReady = (callback) => {
-    if (document.readyState != "loading") {
-        callback();
-    }
-    else if (document.addEventListener) {
-        document.addEventListener("DOMContentLoaded", callback);
-    }
-    else {
-        document.attachEvent("onreadystatechange", function () {
-            if (document.readyState == "complete") {
-                callback();
-            }
-        });
-    }
-};
+searchInput.addEventListener("input", function (event) {
+    const value = event.target.value.toLowerCase()
+    examSeries.forEach(exam => {
+        const isVisible = exam.series.toLowerCase().includes(value)
+        exam.element.classList.toggle("hide", !isVisible)
+    });
+})
 
-onReady(async () => {
-    searchInput.addEventListener("input", function (event) {
-        const value = event.target.value.toLowerCase()
-        examSeries.forEach(exam => {
-            const isVisible = exam.series.toLowerCase().includes(value)
-            exam.element.classList.toggle("hide", !isVisible)
-        });
-    })
-
-    document.addEventListener("click", function (event) {
-        if (event.target.className == "series_card") {
-            var seriesName = event.target.id
-            console.log(seriesName)
-        } else if (event.target.className == "series_name" || event.target.className == "series_views") {
-            var seriesName = event.target.parentElement.id
-            console.log(seriesName)
+document.addEventListener("click", function (event) {
+    if (event.target.className == "main_card") {
+        event.target.querySelector("[data-main-card-views]").classList.toggle("hide")
+        for (const view of event.target.parentElement.querySelector("[data-views-cards]").children) {
+            view.classList.toggle("hide")
         }
-    })
-});
+    } else if (event.target.className == "main_card_name" || event.target.className == "main_card_views") {
+        event.target.parentElement.querySelector("[data-main-card-views]").classList.toggle("hide")
+        for (const view of event.target.parentElement.parentElement.querySelector("[data-views-cards]").children) {
+            view.classList.toggle("hide")
+        }
+    }
+})
 
 fetch("./exams.json")
     .then((res) => res.json())
     .then((data) => {
         examSeries = data.map(exam => {
-            const card = examCardTemplate.content.cloneNode(true).children[0]
-            card.id = exam["Series"]
+            const exam_cards = examCardTemplate.content.cloneNode(true).children[0]
 
-            const series = card.querySelector("[data-series]")
-            const views = card.querySelector("[data-views]")
-
+            const main_card = exam_cards.querySelector("[data-main-card]")
+            main_card.id = exam["Series"]
+            const series = main_card.querySelector("[data-main-card-name]")
+            const views = main_card.querySelector("[data-main-card-views]")
             series.textContent = exam["Series"]
             views.textContent = exam["Views"]
 
-            examCardsContainer.append(card)
-            return { series: exam["Series"], views: exam["Views"], element: card }
+            const views_cards = exam_cards.querySelector("[data-views-cards]")
+            exam["Views"].split(", ").forEach(view => {
+                const node = document.createElement("div")
+                const textnode = document.createTextNode(view)
+                node.appendChild(textnode)
+                node.classList.add("hide")
+                node.classList.add("view_card")
+                views_cards.appendChild(node)
+            });
+
+            exam_cards.appendChild(main_card)
+            exam_cards.appendChild(views_cards)
+
+            examCardsContainer.append(exam_cards)
+            return { series: exam["Series"], views: exam["Views"], element: exam_cards }
         });
     });
