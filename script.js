35c8cc6f99d237bda56642fec6230bf3e4319629
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2023-02-09 16:35:36 -0800

    restructured json

--- script.js
+++ script.js
@@ -12,56 +12,6 @@ const page_cr = document.getElementById("cr")
 const page_respiration = document.getElementById("respiration")
 const page_evaluation_criteria = document.getElementById("evaluation_criteria")
 
-let examSeries = []
-let exams = []
-
-searchInput.addEventListener("input", function (event) {
-    const value = event.target.value.toLowerCase()
-    examSeries.forEach(exam => {
-        const isVisible = exam.series.toLowerCase().includes(value)
-        exam.element.classList.toggle("hide", !isVisible)
-    });
-})
-
-document.addEventListener("click", function (event) {
-    if (event.target.className == "view_card") {
-        exams.forEach(exam => {
-            if (exam["Name"] == event.target.id) {
-                // Exam Name
-                page_name.innerHTML = exam["Name"]
-
-                // Exam Image
-                page_img.src = "./img/" + exam["Part"].toLowerCase() + "/" + exam["View"] + ".jpg"
-
-                // Clinical Indications
-                populateListFromArray(page_clinical_indications, exam["Clinical Indications"])
-
-                // Technical Factors
-                populateListFromObject(page_technical_factors, exam["Technical Factors"])
-
-                // Patient Position
-                populateListFromArray(page_patient_position, exam["Patient Position"])
-
-                // Part Position
-                populateListFromArray(page_part_position, exam["Part Position"])
-
-                // CR
-                populateListFromArray(page_cr, exam["CR"])
-
-                // Respiration
-                populateListFromArray(page_respiration, exam["Respiration"])
-
-                // Evaluation Criteria
-                populateListFromObject(page_evaluation_criteria, exam["Evaluation Criteria"])
-            }
-        });
-
-        document.getElementById("column_one").classList.remove("hide")
-        document.getElementById("column_two").classList.remove("hide")
-        document.getElementById("intro").classList.add("hide")
-    }
-})
-
 function populateListFromArray(list, array) {
     list.innerHTML = ""
     array.forEach(item => {
@@ -83,34 +33,97 @@ function populateListFromObject(list, object) {
     }
 }
 
-fetch("./exams.json")
-    .then((res) => res.json())
-    .then((data) => {
-        examSeries = data.map(series => {
-            series["Exams"].forEach(element => {
-                exams.push(element)
-            });
+// Left side nav bar stuff
+let exams = []
 
-            const exam_cards = examCardTemplate.content.cloneNode(true).children[0]
+function getExams() {
+    fetch("./exams/exams.json")
+        .then((res) => res.json())
+        .then((data) => {
+            exams = data.map(series => {
+                const exam_cards = examCardTemplate.content.cloneNode(true).children[0]
 
-            const main_card = exam_cards.querySelector("[data-main-card]")
-            main_card.id = series["Part"]
-            const seriesElement = main_card.querySelector("[data-main-card-name]")
-            seriesElement.textContent = series["Part"]
+                const main_card = exam_cards.querySelector("[data-main-card]")
+                main_card.id = series["Part"]
+                const seriesElement = main_card.querySelector("[data-main-card-name]")
+                seriesElement.textContent = series["Part"]
 
-            const viewsElement = main_card.querySelector("[data-main-card-views]")
-            series["Exams"].forEach(exam => {
-                const node = document.createElement("div")
-                const textnode = document.createTextNode(exam["View"])
-                node.appendChild(textnode)
-                node.classList.add("view_card")
-                node.id = exam["View"] + " " + series["Part"]
-                viewsElement.appendChild(node)
-            });
+                const viewsElement = main_card.querySelector("[data-main-card-views]")
+                series["Views"].forEach(view => {
+                    const node = document.createElement("div")
+                    const textnode = document.createTextNode(view)
+                    node.appendChild(textnode)
+                    node.classList.add("view_card")
+                    node.dataset.part = series["Part"]
+                    node.dataset.view = view
+                    viewsElement.appendChild(node)
+                });
 
-            exam_cards.appendChild(main_card)
+                exam_cards.appendChild(main_card)
+                examCardsContainer.append(exam_cards)
+                return { part: series["Part"], element: exam_cards }
+            })
+        })
 
-            examCardsContainer.append(exam_cards)
-            return { series: series["Part"], views: series["Views"], element: exam_cards }
+    searchInput.addEventListener("input", function (event) {
+        const value = event.target.value.toLowerCase()
+        exams.forEach(exam => {
+            const isVisible = exam.part.toLowerCase().includes(value)
+            exam.element.classList.toggle("hide", !isVisible)
         });
-    });
+    })
+}
+
+
+
+function generatePage() {
+    document.addEventListener("click", function (event) {
+        if (event.target.className == "view_card") {
+            var part = event.target.dataset.part.toLowerCase()
+            var view = event.target.dataset.view
+
+            fetch("./exams/" + part + "/" + part + ".json")
+                .then((res) => res.json())
+                .then((data) => {
+                    data.forEach(exam => {
+                        // View stays uppercase, as opposed to part
+                        if (exam["View"] == view) {
+                            // Exam Name
+                            page_name.innerHTML = exam["Name"]
+
+                            // Exam Image
+                            page_img.src = "./exams/" + part + "/" + exam["View"] + ".jpg"
+
+                            // Clinical Indications
+                            populateListFromArray(page_clinical_indications, exam["Clinical Indications"])
+
+                            // Technical Factors
+                            populateListFromObject(page_technical_factors, exam["Technical Factors"])
+
+                            // Patient Position
+                            populateListFromArray(page_patient_position, exam["Patient Position"])
+
+                            // Part Position
+                            populateListFromArray(page_part_position, exam["Part Position"])
+
+                            // CR
+                            populateListFromArray(page_cr, exam["CR"])
+
+                            // Respiration
+                            populateListFromArray(page_respiration, exam["Respiration"])
+
+                            // Evaluation Criteria
+                            populateListFromObject(page_evaluation_criteria, exam["Evaluation Criteria"])
+                        }
+                    })
+                })
+
+            document.getElementById("column_one").classList.remove("hide")
+            document.getElementById("column_two").classList.remove("hide")
+            document.getElementById("intro").classList.add("hide")
+        }
+    })
+}
+
+getExams()
+generatePage()
