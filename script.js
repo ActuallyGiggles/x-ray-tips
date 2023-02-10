8785c68f619a527b0c96900a51905738e38e5f3b
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2023-02-10 11:17:09 -0800

    light mode

--- script.js
+++ script.js
@@ -50,13 +50,18 @@ function getExams() {
 
                 const viewsElement = main_card.querySelector("[data-main-card-views]")
                 series["Views"].forEach(view => {
-                    const node = document.createElement("div")
-                    node.classList.add("view_card")
+                    const div = document.createElement("div")
+                    div.classList.add("view_card")
+                    const textDiv = document.createElement("div")
+                    textDiv.classList.add("view_card_label")
+                    textDiv.dataset.part = series["Part"]
+                    textDiv.dataset.view = view
                     const textnode = document.createTextNode(view)
-                    node.appendChild(textnode)
-                    node.dataset.part = series["Part"]
-                    node.dataset.view = view
-                    viewsElement.appendChild(node)
+                    textDiv.appendChild(textnode)
+                    div.appendChild(textDiv)
+                    div.dataset.part = series["Part"]
+                    div.dataset.view = view
+                    viewsElement.appendChild(div)
                 });
 
                 exam_cards.appendChild(main_card)
@@ -76,62 +81,90 @@ function getExams() {
 
 function generatePage() {
     document.addEventListener("click", function (event) {
-        if (event.target.className == "nav_button") {
-            if (event.target.id == "home") {
-                document.getElementById("intro_content").innerHTML = "Search for an exam at the upper left.<br>Or...<br>Pick an exam to review."
-            } else if (event.target.id == "about") {
-                document.getElementById("intro_content").innerHTML = `Hi! My name is Mark!<br><br>I created this website as an x-ray student for x-ray students.<br>This is meant to be used if you quickly need to brush up on some exam you haven't done in a while.<br><br>Feel free to tell me what you think so far by emailing me at <span style="text-decoration: underline;">mklymenko007@gmail.com</span>.<br><br>Have a radioactive day!`
+        if (event.target.id == "home") {
+            document.getElementById("intro_content").innerHTML = "Search for an exam at the upper left.<br>Or...<br>Pick an exam to review."
+            document.getElementById("column_one").classList.add("hide")
+            document.getElementById("column_two").classList.add("hide")
+        } else if (event.target.id == "about") {
+            document.getElementById("intro_content").innerHTML = `Hi! My name is Mark!<br><br>I created this website as an x-ray student for x-ray students.<br>This is meant to be used if you quickly need to brush up on some exam you haven't done in a while.<br><br>Feel free to tell me what you think so far by emailing me at <span style="text-decoration: underline;">xray.positioning.tips@gmail.com</span>.<br><br>Have a radioactive day!`
+            document.getElementById("column_one").classList.add("hide")
+            document.getElementById("column_two").classList.add("hide")
+        } else if (event.target.id == "theme") {
+            document.body.classList.toggle("body_light_mode")
+            document.getElementById("left_bar").classList.toggle("left_bar_light_mode")
+            document.getElementById("page_content").classList.toggle("page_content_light_mode")
+
+            if (document.getElementById("gitImage").src.includes("light.png")) {
+                document.getElementById("gitImage").src = "img/github_dark.png"
+            } else if (document.getElementById("gitImage").src.includes("dark.png")) {
+                document.getElementById("gitImage").src = "img/github_light.png"
             }
 
-            document.getElementById("column_one").classList.add("hide")
-            document.getElementById("column_two").classList.add("hide")
+            var array = document.getElementsByClassName("nav_button")
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                element.classList.toggle("button_card_light_mode")
+            }
+            var array = document.getElementsByClassName("view_card")
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                element.classList.toggle("button_card_light_mode")
+            }
+            var array = document.getElementsByClassName("main_card")
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                element.classList.toggle("main_card_light_mode")
+            }
         }
 
-        if (event.target.className == "view_card") {
-            var part = event.target.dataset.part.toLowerCase()
-            var view = event.target.dataset.view
+        for (let index = 0; index < event.target.className.split(" ").length; index++) {
+            const className = event.target.className.split(" ")[index];
+            if (className == "view_card" || className == "view_card_label") {
+                var part = event.target.dataset.part.toLowerCase()
+                var view = event.target.dataset.view
 
-            document.getElementById("column_one").classList.add("hide")
-            document.getElementById("column_two").classList.add("hide")
+                document.getElementById("column_one").classList.add("hide")
+                document.getElementById("column_two").classList.add("hide")
 
-            fetch("./exams/" + part + "/" + part + ".json")
-                .then((res) => res.json())
-                .then((data) => {
-                    data.forEach(exam => {
-                        // View stays uppercase, as opposed to part
-                        if (exam["View"] == view) {
-                            // Exam Name
-                            page_name.innerHTML = exam["Name"]
+                fetch("./exams/" + part + "/" + part + ".json")
+                    .then((res) => res.json())
+                    .then((data) => {
+                        data.forEach(exam => {
+                            // View stays uppercase, as opposed to part
+                            if (exam["View"] == view) {
+                                // Exam Name
+                                page_name.innerHTML = exam["Name"]
 
-                            // Exam Image
-                            page_img.src = "./exams/" + part + "/" + exam["View"] + ".jpg"
+                                // Exam Image
+                                page_img.src = "./exams/" + part + "/" + exam["View"] + ".jpg"
 
-                            // Clinical Indications
-                            populateListFromArray(page_clinical_indications, exam["Clinical Indications"])
+                                // Clinical Indications
+                                populateListFromArray(page_clinical_indications, exam["Clinical Indications"])
 
-                            // Technical Factors
-                            populateListFromObject(page_technical_factors, exam["Technical Factors"])
+                                // Technical Factors
+                                populateListFromObject(page_technical_factors, exam["Technical Factors"])
 
-                            // Patient Position
-                            populateListFromArray(page_patient_position, exam["Patient Position"])
+                                // Patient Position
+                                populateListFromArray(page_patient_position, exam["Patient Position"])
 
-                            // Part Position
-                            populateListFromArray(page_part_position, exam["Part Position"])
+                                // Part Position
+                                populateListFromArray(page_part_position, exam["Part Position"])
 
-                            // CR
-                            populateListFromArray(page_cr, exam["CR"])
+                                // CR
+                                populateListFromArray(page_cr, exam["CR"])
 
-                            // Respiration
-                            populateListFromArray(page_respiration, exam["Respiration"])
+                                // Respiration
+                                populateListFromArray(page_respiration, exam["Respiration"])
 
-                            // Evaluation Criteria
-                            populateListFromObject(page_evaluation_criteria, exam["Evaluation Criteria"])
-                        }
+                                // Evaluation Criteria
+                                populateListFromObject(page_evaluation_criteria, exam["Evaluation Criteria"])
+                            }
+                        })
                     })
-                })
 
-            document.getElementById("column_one").classList.remove("hide")
-            document.getElementById("column_two").classList.remove("hide")
+                document.getElementById("column_one").classList.remove("hide")
+                document.getElementById("column_two").classList.remove("hide")
+            }
         }
     })
 }
